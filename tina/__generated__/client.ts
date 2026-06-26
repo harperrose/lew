d2914c4244b36bde420d47c79c8e99d84f270059
harperrose <101375619+harperrose@users.noreply.github.com>
2026-06-26 11:49:31 -0400

    updated styling

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '9eac08e86cbe1fe631dec2cef3eb64ac8872b4cb', queries,  });
 export default client;
   
